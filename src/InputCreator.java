0999e06869ce5216ae07a33e8dd40b3b091787c6
varun <varunparbhakar@yahoo.in>
2022-04-10 15:38:31 -0700

    Changed Bomb Constructor

--- src/InputCreator.java
+++ src/InputCreator.java
@@ -1,5 +1,21 @@
+import java.io.File;
+
 public class InputCreator {
     public static void main(String[] args) {
+        int max = 10;
+        int min = 1;
+        int range = max - min + 1;
+        int myRows = (int)(Math.random() * range) + min;
+        int myColumns = (int)(Math.random() * range) + min;
+
+        double bombRatio = 0.5;
+        int totalBombs = (int)((myRows * myColumns) * bombRatio);
+        System.out.println("Rows: " + myRows);
+        System.out.println("Columns: " + myColumns);
+        System.out.println("Map Size: " + (myRows * myColumns));
+        System.out.println("Bombs: " + totalBombs);
+
+
 
     }
 }
