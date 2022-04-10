55060e7c8f302b28cca9d53a0c41687c416fa361
varun <varunparbhakar@yahoo.in>
2022-04-10 16:46:45 -0700

    Changed the name of the main directory, added the working create a minefield class

--- src/InputCreator.java
+++ src/InputCreator.java
@@ -1,8 +1,15 @@
 import java.io.File;
+import java.util.Arrays;
 
 public class InputCreator {
     public static void main(String[] args) {
-        int max = 10;
+        for (int i = 0; i < 10; i++) {
+            mapCreator();
+        }
+        System.out.println("0 0");
+    }
+    public static void mapCreator() {
+        int max = 250;
         int min = 1;
         int range = max - min + 1;
         int myRows = (int)(Math.random() * range) + min;
@@ -10,12 +17,34 @@ public class InputCreator {
 
         double bombRatio = 0.5;
         int totalBombs = (int)((myRows * myColumns) * bombRatio);
-        System.out.println("Rows: " + myRows);
-        System.out.println("Columns: " + myColumns);
-        System.out.println("Map Size: " + (myRows * myColumns));
-        System.out.println("Bombs: " + totalBombs);
+
+//        System.out.println("Rows: " + myRows);
+//        System.out.println("Columns: " + myColumns);
+//        System.out.println("Map Size: " + (myRows * myColumns));
+//        System.out.println("Bombs: " + totalBombs);
+
+        char[][] myMap = new char[myRows][myColumns];
+        System.out.println(myRows + " " + myColumns);
+
+        while (totalBombs > 0) {
+            for (int i = 0; i < myRows; i++) {
+                for (int j = 0; j < myColumns; j++) {
+                    if(Math.random() < bombRatio) {
+                        myMap[i][j] = '*';
+                        totalBombs--;
+                    } else {
+                        myMap[i][j] = '.';
+                    }
+                }
+            }
+        }
 
 
-
+        for (char[] row: myMap) {
+            for (char character : row)  {
+                System.out.print(character);
+            }
+            System.out.println();
+        }
     }
 }
