efafc189cd3861e7e113098c589d5c5d973fcb07
Yaci <bennoury@uw.edu>
2022-04-11 17:28:01 -0700

    Start of MineMapTest --Removed println. --Merged with master.

--- src/Main.java
+++ src/Main.java
@@ -15,14 +15,16 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
         MineMap testInsertMap = new MineMap(1, 5);
-        testInsertMap.insertBomb(0,0);
-        testInsertMap.insertIndicator(0,1);
-        testInsertMap.insertIndicator(0,2);
-        testInsertMap.insertIndicator(0,3);
-        testInsertMap.insertIndicator(0,4);
+//        testInsertMap.insertBomb(0,0);
+//        testInsertMap.insertIndicator(0,1);
+//        testInsertMap.insertIndicator(0,2);
+//        testInsertMap.insertIndicator(0,3);
+//        testInsertMap.insertIndicator(0,4);
+//
+//        testInsertMap.indicatorSetter();
+//        testInsertMap.printMapWithIndicators();
 
-        testInsertMap.indicatorSetter();
-        testInsertMap.printMapWithIndicators();
+
 
 
 //        File myFile = new File("team_minesweeper_input.txt");
