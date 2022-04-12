bb5ec4b85224b00dc9c3a105f4595ed9d4500ffd
Tinytin06 <52684983+Tinytin06@users.noreply.github.com>
2022-04-11 18:32:51 -0700

    sysin

--- Tests/MainTest.java
+++ Tests/MainTest.java
@@ -1,23 +1,30 @@
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
-    /**
-     * please use file redirection for this method
-     * @throws FileNotFoundException
-     */
-    @org.junit.jupiter.api.Test
-    void main() throws FileNotFoundException {
-    Main main = new Main();
-    main.main(new String[]{});
-    }
 
-    @org.junit.jupiter.api.Test
-    void driver() {
-    }
+        /**
+         * please use file redirection for this method
+         * redirection should be input.txt to output.txt
+         * @throws FileNotFoundException
+         */
+        @org.junit.jupiter.api.Test
+        void main() throws FileNotFoundException {
+            File File1 = new File("output.txt");
+            String File2
+            assertEquals("The files differ!" ,Files.toString(File1, Charset.forName("UTF-8")), Files.toString("output.txt", Charset.forName("UTF-8")));
+        }
 
-    @org.junit.jupiter.api.Test
-    void mapBuilder() {
+        @org.junit.jupiter.api.Test
+        void driver() {
+        }
+
+        @org.junit.jupiter.api.Test
+        void mapBuilder() {
+        }
+
     }
-}
