ef8ba9bc5cc1fe5768bfc5dfd26839a9b5e7a13d
varun <varunparbhakar@yahoo.in>
2022-04-11 18:32:11 -0700

    UPdateed main

--- src/Main.java
+++ src/Main.java
@@ -34,9 +34,6 @@ public class Main {
             counter++;
         }
 
-        File myFile = new File("team_minesweeper_input.txt");
-        Scanner fileScanner = new Scanner(System.in);
-        int counter = 1;
 
 
     }
